756bf19f43c436bc22fa87f4bf211ef5f3382e58
LeLuongSWE <luongswe@gmail.com>
2025-09-18 14:46:11 +0000

    final project initiation

---
+++ source_code/backend/final_project/src/main/java/vn/edu/hust/final_project/FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hust.final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectApplication.class, args);
+	}
+
+}
